7d65896b342e49f88823069fac5039ab8508b510
PurestDuck <69611453+PurestDuck@users.noreply.github.com>
2025-06-16 11:34:02 -0700

    Update models.py
    Added better support for multiple training images

--- models.py
+++ models.py
@@ -19,16 +19,25 @@ features_func = partial(
     sigma_max=sigma_max,
 )
 
-#roiX = [
-#roiY
+#[250:340,0:2071]
+training_labels = []
+test_img_features = []
+trainNum = np.array([0,0,751,980,2970])
+#trainNum = np.array([0,0,375,550,751,980])
+#roiX = np.array([[130,200],[250,340],[120,200],[50,110],[250,340],[30,130]])
+#roiY = np.array([[0,760],[0,760],[1670,2071],[380,480],[0,2071],[0,2071]])
+roiX = np.array([[130,200],[250,340],[250,340],[0,340],[0,340]])
+roiY = np.array([[0,760],[0,760],[0,2071],[0,2071],[0,2071]])
 
-training_imgOne = io.imread(pred_file)[0]
+for x in range(len(trainNum)):
+   training_img = io.imread(pred_file)[trainNum[x]]
+   roi = io.imread(raw_file)[trainNum[x] ][roiX[x][0]:roiX[x][1],roiY[x][0]:roiY[x][1]]
+   mask = training_img[roiX[x][0]:roiX[x][1],roiY[x][0]:roiY[x][1]]
+   training_labels.append( mask.astype(np.uint8) + 1 )
+   test_img_features.append( features_func(roi) )
+   #plt.imshow(mask)
+   #plt.show()
 
-roi = io.imread(raw_file)[0][130:200,0:760]
-mask = training_imgOne[130:200,0:760]
-training_labels = mask.astype(np.uint8) + 1
-
-test_img_features = features_func(roi)
 
 def convert_to_binary(image):
     edited = image
@@ -41,11 +50,14 @@ def convert_to_binary(image):
     return edited
 
 def mlp_model():
-    mlp_clf = MLPClassifier(hidden_layer_sizes=800,random_state=1, max_iter=300)
-    mlp_clf = future.fit_segmenter(training_labels, test_img_features, mlp_clf)
+    mlp_clf = MLPClassifier(hidden_layer_sizes=800,random_state=1, max_iter=300,warm_start=True)
+    for x in range(len(training_labels)):
+        mlp_clf = future.fit_segmenter(training_labels[x], test_img_features[x], mlp_clf)
     return mlp_clf
 
 def forest_model():
-    forest_clf = RandomForestClassifier(n_estimators=100, n_jobs=-1, max_depth=10, max_samples=0.05)
-    forest_clf = future.fit_segmenter(training_labels, test_img_features, forest_clf)
+    forest_clf = RandomForestClassifier(n_estimators=100, n_jobs=-1, max_depth=10, max_samples=0.05,warm_start=True)
+    for x in range(len(training_labels)):
+        forest_clf.n_estimators = forest_clf.n_estimators + 100
+        forest_clf = future.fit_segmenter(training_labels[x], test_img_features[x], forest_clf)
     return forest_clf
