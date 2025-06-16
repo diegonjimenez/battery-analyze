c9eaf6170b73f62659603fdd93a9505931943f3c
PurestDuck <69611453+PurestDuck@users.noreply.github.com>
2025-06-16 09:57:52 -0700

    Update models.py
    Stable Ver / better training

--- models.py
+++ models.py
@@ -1,21 +1,15 @@
 import numpy as np
-from skimage import io, filters
-from skimage import morphology, feature, future
+import matplotlib.pyplot as plt
+from skimage import io, filters, feature, future, morphology
+from sklearn import metrics
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.neural_network import MLPClassifier
 from functools import partial
 
 raw_file = "images/8bitRawXCT.tif"
 pred_file = "images/binaryPredicted.tif"
-
-training_thresholded_img = io.imread(raw_file)[0] > filters.threshold_triangle(io.imread(raw_file)[0])
-
-roi = io.imread(raw_file)[0][130:200,0:760]
-mask = training_thresholded_img[130:200,0:760]
-training_labels = mask.astype(np.uint8) + 1
 sigma_min = 1
 sigma_max = 16
-
 features_func = partial(
     feature.multiscale_basic_features,
     intensity=True,
@@ -25,8 +19,26 @@ features_func = partial(
     sigma_max=sigma_max,
 )
 
+#roiX = [
+#roiY
+
+training_imgOne = io.imread(pred_file)[0]
+
+roi = io.imread(raw_file)[0][130:200,0:760]
+mask = training_imgOne[130:200,0:760]
+training_labels = mask.astype(np.uint8) + 1
+
 test_img_features = features_func(roi)
 
+def convert_to_binary(image):
+    edited = image
+    for x in range(len(edited)):
+        for y in range(len(edited[x])):
+            if edited[x][y] == 2:
+                edited[x][y] = 1
+            elif edited[x][y] == 1:
+                edited[x][y] = 0
+    return edited
 
 def mlp_model():
     mlp_clf = MLPClassifier(hidden_layer_sizes=800,random_state=1, max_iter=300)
