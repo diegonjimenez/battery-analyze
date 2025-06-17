596a05628d336046f3209317f48dd5314cb279dd
PurestDuck <69611453+PurestDuck@users.noreply.github.com>
2025-06-17 16:21:51 -0700

    Update models.py
    Speed performance / slight performance downgrade (~4%) due to downgrading of training img

--- models.py
+++ models.py
@@ -8,8 +8,10 @@ from sklearn.neural_network import MLPClassifier
 from functools import partial
 
 #SEC 1
-raw_file = "images/8bitRawXCT.tif"
-pred_file = "images/binaryPredicted.tif"
+
+downsample = 2
+raw_file = io.imread("images/8bitRawXCT.tif")
+pred_file = io.imread("images/binaryPredicted.tif")
 sigma_min = 1
 sigma_max = 16
 features_func = partial(
@@ -29,8 +31,8 @@ predImgStack = []
 trainNum = np.array([0,751,980,1900,2970])
 
 for x in range(len(trainNum)):
-   rawImgStack.append(io.imread(raw_file)[trainNum[x]])
-   predImgStack.append(io.imread(pred_file)[trainNum[x]])
+   rawImgStack.append( raw_file[trainNum[x]][::downsample,::downsample] )
+   predImgStack.append( pred_file[trainNum[x]][::downsample,::downsample] )
 
 predImgStack = np.vstack(predImgStack)
 rawImgStack = np.vstack(rawImgStack)
@@ -38,6 +40,12 @@ rawImgStack = np.vstack(rawImgStack)
 training_labels = predImgStack.astype(np.uint8) + 1
 test_img_features = features_func(rawImgStack)
 
+#f,ax = plt.subplots(1,2, figsize = (16,9))
+#ax[0].imshow(predImgStack, cmap = "gray")
+#ax[1].imshow(rawImgStack, cmap = "gray")
+#plt.show()
+
+print("Passed Section 1")
 
 # SEC 2
 def convert_to_binary(image):
@@ -53,9 +61,12 @@ def convert_to_binary(image):
 def mlp_model():
     mlp_clf = MLPClassifier(hidden_layer_sizes=800,random_state=1, max_iter=300,warm_start=True)
     mlp_clf = future.fit_segmenter(training_labels, test_img_features, mlp_clf)
+    print("Finished mlp forest")
     return mlp_clf
 
 def forest_model():
     forest_clf = RandomForestClassifier(n_estimators=100, n_jobs=-1, max_depth=10, max_samples=0.05,warm_start=True)
     forest_clf = future.fit_segmenter(training_labels, test_img_features, forest_clf)
+    print("Finished training forest")
     return forest_clf
+print("Passed Section 2")
