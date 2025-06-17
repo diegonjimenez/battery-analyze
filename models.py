fac3efb87428607a222745fb15d524e8e66c138a
PurestDuck <69611453+PurestDuck@users.noreply.github.com>
2025-06-17 15:10:39 -0700

    Update models.py
    Stacked Images

--- models.py
+++ models.py
@@ -2,10 +2,12 @@ import numpy as np
 import matplotlib.pyplot as plt
 from skimage import io, filters, feature, future, morphology
 from sklearn import metrics
+from skimage.transform import rescale
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.neural_network import MLPClassifier
 from functools import partial
 
+#SEC 1
 raw_file = "images/8bitRawXCT.tif"
 pred_file = "images/binaryPredicted.tif"
 sigma_min = 1
@@ -20,25 +22,24 @@ features_func = partial(
 )
 
 #[250:340,0:2071]
-training_labels = []
-test_img_features = []
-trainNum = np.array([0,0,751,980,2970])
-#trainNum = np.array([0,0,375,550,751,980])
-#roiX = np.array([[130,200],[250,340],[120,200],[50,110],[250,340],[30,130]])
-#roiY = np.array([[0,760],[0,760],[1670,2071],[380,480],[0,2071],[0,2071]])
-roiX = np.array([[130,200],[250,340],[250,340],[0,340],[0,340]])
-roiY = np.array([[0,760],[0,760],[0,2071],[0,2071],[0,2071]])
+
+rawImgStack = []
+predImgStack = []
+
+trainNum = np.array([0,751,980,1900,2970])
 
 for x in range(len(trainNum)):
-   training_img = io.imread(pred_file)[trainNum[x]]
-   roi = io.imread(raw_file)[trainNum[x] ][roiX[x][0]:roiX[x][1],roiY[x][0]:roiY[x][1]]
-   mask = training_img[roiX[x][0]:roiX[x][1],roiY[x][0]:roiY[x][1]]
-   training_labels.append( mask.astype(np.uint8) + 1 )
-   test_img_features.append( features_func(roi) )
-   #plt.imshow(mask)
-   #plt.show()
+   rawImgStack.append(io.imread(raw_file)[trainNum[x]])
+   predImgStack.append(io.imread(pred_file)[trainNum[x]])
+
+predImgStack = np.vstack(predImgStack)
+rawImgStack = np.vstack(rawImgStack)
+
+training_labels = predImgStack.astype(np.uint8) + 1
+test_img_features = features_func(rawImgStack)
 
 
+# SEC 2
 def convert_to_binary(image):
     edited = image
     for x in range(len(edited)):
@@ -51,13 +52,10 @@ def convert_to_binary(image):
 
 def mlp_model():
     mlp_clf = MLPClassifier(hidden_layer_sizes=800,random_state=1, max_iter=300,warm_start=True)
-    for x in range(len(training_labels)):
-        mlp_clf = future.fit_segmenter(training_labels[x], test_img_features[x], mlp_clf)
+    mlp_clf = future.fit_segmenter(training_labels, test_img_features, mlp_clf)
     return mlp_clf
 
 def forest_model():
     forest_clf = RandomForestClassifier(n_estimators=100, n_jobs=-1, max_depth=10, max_samples=0.05,warm_start=True)
-    for x in range(len(training_labels)):
-        forest_clf.n_estimators = forest_clf.n_estimators + 100
-        forest_clf = future.fit_segmenter(training_labels[x], test_img_features[x], forest_clf)
+    forest_clf = future.fit_segmenter(training_labels, test_img_features, forest_clf)
     return forest_clf
