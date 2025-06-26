884e6e222b4e24f38c0c01d16de0861855a49e3d
Diego Jimenez <69611453+diegonjimenez@users.noreply.github.com>
2025-06-26 10:15:11 -0700

    Update models.py
    Stable V2

--- models.py
+++ models.py
@@ -1,19 +1,23 @@
+#SEC 1: Setting up modules
 import numpy as np
 import matplotlib.pyplot as plt
 from skimage import io, filters, feature, future, morphology
+import pandas as pd
 from sklearn import metrics
 from skimage.transform import rescale
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.neural_network import MLPClassifier
 from functools import partial
+import tifffile as tiff
+from tifffile import TiffFile
 
-#SEC 1
+#SEC 2: Setting up control vars
+
+future_index = 121#np.random.randint(0, 3849)
 
 downsample = 2
-raw_file = io.imread("images/8bitRawXCT.tif")
-pred_file = io.imread("images/binaryPredicted.tif")
 sigma_min = 1
-sigma_max = 16
+sigma_max = 20
 features_func = partial(
     feature.multiscale_basic_features,
     intensity=True,
@@ -23,29 +27,35 @@ features_func = partial(
     sigma_max=sigma_max,
 )
 
-#[250:340,0:2071]
 
+# SEC 3: Setting up images
+raw_file = "images/8bitRawXCT.tif"
+pred_file = "images/binaryPredicted.tif"
 rawImgStack = []
 predImgStack = []
 
-trainNum = np.array([0,751,980,1900,2970])
+trainNum = np.array([0,751,980,2970]) #
 
-for x in range(len(trainNum)):
-   rawImgStack.append( raw_file[trainNum[x]][::downsample,::downsample] )
-   predImgStack.append( pred_file[trainNum[x]][::downsample,::downsample] )
+with TiffFile(raw_file) as raw, TiffFile(pred_file) as pred:
+    imgRaw = raw.pages[future_index].asarray()
+    imgPred = pred.pages[future_index].asarray()
+    for x in range(len(trainNum)):
+       rawImgStack.append(raw.pages[trainNum[x]].asarray()[::downsample,::downsample] )
+       predImgStack.append(pred.pages[trainNum[x]].asarray()[::downsample,::downsample] )
 
 predImgStack = np.vstack(predImgStack)
 rawImgStack = np.vstack(rawImgStack)
 
+
 training_labels = predImgStack.astype(np.uint8) + 1
 test_img_features = features_func(rawImgStack)
 
-#f,ax = plt.subplots(1,2, figsize = (16,9))
-#ax[0].imshow(predImgStack, cmap = "gray")
-#ax[1].imshow(rawImgStack, cmap = "gray")
-#plt.show()
+print("Training_labels shape is ", training_labels.shape)
+print("test_img_features shape is ", test_img_features.shape)
 
-print("Passed Section 1")
+#SEC 3: Models!
+
+print("Passed Section 1") #OPTIONAL
 
 # SEC 2
 def convert_to_binary(image):
@@ -59,13 +69,13 @@ def convert_to_binary(image):
     return edited
 
 def mlp_model():
-    mlp_clf = MLPClassifier(hidden_layer_sizes=800,random_state=1, max_iter=300,warm_start=True)
+    mlp_clf = MLPClassifier(hidden_layer_sizes=400,random_state=1, max_iter=300)
     mlp_clf = future.fit_segmenter(training_labels, test_img_features, mlp_clf)
     print("Finished mlp forest")
     return mlp_clf
 
 def forest_model():
-    forest_clf = RandomForestClassifier(n_estimators=100, n_jobs=-1, max_depth=10, max_samples=0.05,warm_start=True)
+    forest_clf = RandomForestClassifier(n_estimators=120, n_jobs=-1, max_depth=16, max_samples=0.02) #og n_estimators=100, n_jobs=-1, max_depth=10, max_samples=0.05
     forest_clf = future.fit_segmenter(training_labels, test_img_features, forest_clf)
     print("Finished training forest")
     return forest_clf
