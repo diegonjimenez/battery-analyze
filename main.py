0519ae5bce6f177a9352529262f2d05b0e81788e
Diego Jimenez <69611453+diegonjimenez@users.noreply.github.com>
2025-06-26 10:14:41 -0700

    Update main.py
    Stable V2

--- main.py
+++ main.py
@@ -1,23 +1,24 @@
 from models import *
 import time #OPTIONAL
-import pandas as pd
+
 
 start_time = time.time() #OPTIONAL
 
+
 ## sets up file and renders only first one
-index = 1239#np.random.randint(0, 3849)
-imgRaw = raw_file[index]
-imgPred = pred_file[index]
+index = future_index#np.random.randint(0, 3849)
+medianImg = filters.median(imgRaw, morphology.footprint_rectangle((3,3)))
 
+# Sets up features for training
 main_img_features = features_func(imgRaw)
-median_img_features = features_func(filters.median(imgRaw, morphology.footprint_rectangle((5,5))))
+median_img_features = features_func(medianImg)
 
+#Gets models and gets results for the diff methods
 forest_model = forest_model()
 mlp_model = mlp_model()
-
 main_forest_result = future.predict_segmenter(main_img_features, forest_model)
 main_mlp_result = future.predict_segmenter(main_img_features, mlp_model)
-main_threshold_result = imgRaw > filters.threshold_triangle(imgRaw)
+main_threshold_result = imgRaw > filters.threshold_otsu(imgRaw)
 main_median_result = future.predict_segmenter(median_img_features, forest_model)
 
 #converting the segmentations to binary for better accuracy on the metrics
@@ -25,13 +26,11 @@ main_forest_result = convert_to_binary(main_forest_result)
 main_mlp_result = convert_to_binary(main_mlp_result)
 main_median_result = convert_to_binary(main_median_result)
 
-
 #median 3x3 rec for median filter - dani
 
 # Setting Graph
-#python pillow for imgs
 
-print("Index is ", index)
+print("Index is ", index) #OPTIONAL
 
 f, ax = plt.subplots(3, 2, figsize=(16, 9))
 ax[0][0].imshow(imgRaw, cmap='gray')
@@ -50,9 +49,15 @@ ax[2][0].imshow(main_mlp_result,cmap='gray')
 ax[2][0].set_title("MLP Image")
 
 ax[2][1].imshow(main_median_result, cmap = "gray")
-ax[2][1].set_title("Forest w/ Median Filter")
+ax[2][1].set_title("Forest w/ Gaussian before")
 
 #Determining metrics for each method
+
+threshold_precision = metrics.accuracy_score(imgPred.flatten(), main_threshold_result.flatten())
+forest_precision = metrics.accuracy_score(imgPred.flatten(), main_forest_result.flatten())
+mlp_precision = metrics.accuracy_score(imgPred.flatten(), main_mlp_result.flatten())
+median_precision = metrics.accuracy_score(imgPred.flatten(), main_median_result.flatten())
+
 
 threshold_iou = metrics.jaccard_score(imgPred.flatten(), main_threshold_result.flatten(),average="binary")
 forest_iou = metrics.jaccard_score(imgPred.flatten(), main_forest_result.flatten(),average="binary")
@@ -65,9 +70,9 @@ mlp_f1 = metrics.f1_score(imgPred.flatten(), main_mlp_result.flatten(),average="
 median_f1 = metrics.f1_score(imgPred.flatten(), main_median_result.flatten(),average="binary")
 
 
-metrics = {"Thresholding":[threshold_iou,threshold_f1], "Forest":[forest_iou,forest_f1],"MLP":[mlp_iou,mlp_f1],"Median Forest":[median_iou,median_f1]}
+metrics = {"Thresholding":[threshold_precision,threshold_iou,threshold_f1], "Forest":[forest_precision,forest_iou,forest_f1],"MLP":[mlp_precision,mlp_iou,mlp_f1],"Median Forest":[median_precision,median_iou,median_f1]}
 
-df = pd.DataFrame(metrics,index=["IoU","F1/Dice"])
+df = pd.DataFrame(metrics,index=["Precision","IoU","F1/Dice"])
 print(df)
 
 end_time = time.time() #OPTIONAL
